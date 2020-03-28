22e546eb1be34f304b8d287d131db35d9d8ddfcf
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-03-28 22:40:49 +0100

    Fix something in data loading for charts

--- packages/HomepageV2/src/components/layout/blocks/Server.js
+++ packages/HomepageV2/src/components/layout/blocks/Server.js
@@ -23,7 +23,6 @@ const Container = styled.section`
   }
 `
 
-
 const Regions = styled.div`
   width: 100%;
   display: flex;
@@ -44,13 +43,13 @@ const Region = styled.div`
   text-transform: uppercase;
   cursor: pointer;
   &:hover{
-    color: ${({theme}) => theme.main}
+    color: ${({ theme }) => theme.main}
   }
 
   &.selected{
-    color: ${({theme}) => theme.main };
+    color: ${({ theme }) => theme.main};
     &:hover{
-      color: ${({theme}) => darken(0.1,theme.main)};
+      color: ${({ theme }) => darken(0.1, theme.main)};
     }
   }
 `
@@ -71,66 +70,75 @@ const Inner = styled.div`
     height: 300px;
   }
 `
-export default function Server(){
-  let dataset = {}
+export default function Server() {
+  const dataset = {}
   const REGIONS = ['eu', 'us', 'au']
-  const [chartData, setChartData] = useState({loading: true})
+  const [chartData, setChartData] = useState({ loading: true })
 
   useEffect(() => {
-    // make eu the default selected region
-    document.querySelector('.region').classList.add('selected')
-    // calculate date 30 days ago
-    let d = new Date()
-    d.setMonth(d.getMonth()-1)
-    // fetch data from different regions
-    REGIONS.map( async (r) => dataset[r] = await getStats(d,r))
-    getChartData()
+    async function fetchData() {
+      // make eu the default selected region
+      document.querySelector('.region').classList.add('selected')
+      // calculate date 30 days ago
+      const d = new Date()
+      d.setMonth(d.getMonth() - 1)
+      // fetch data from different regions
+
+      for (const REGION of REGIONS) {
+        dataset[REGION] = await getStats(d, REGION)
+      }
+
+      await getChartData()
+    }
+    fetchData()
   }, [])
 
-  async function getChartData(){
+  async function getChartData() {
     const regions = await getSelectedRegions()
 
-    if(regions.length>1){
+    if (regions.length > 1) {
       // optellen
     } else {
-      let playerData = []
-      let serverData = []
-      console.log(dataset)
-      dataset[regions[0]].map((regiondata) => {
-        playerData.push({ 'x': regiondata.createdAt, 'y': regiondata.players})
-        serverData.push({ 'x': regiondata.createdAt, 'y': regiondata.servers})
-      })
+      const playerData = []
+      const serverData = []
+      for (const region of regions) {
+        dataset[region].map((regiondata) => {
+          playerData.push({ 'x': regiondata.date, 'y': regiondata.players })
+          serverData.push({ 'x': regiondata.date, 'y': regiondata.servers })
+        })
+      }
 
       setChartData({
         data: [
-          {'id': 'players', 'data': playerData, 'color': 'hsl(197, 70%, 50%)'},
-          { 'id': 'servers', 'data': serverData, 'color': '#f47DA4'}
+          { 'id': 'players', 'data': playerData, 'color': 'hsl(197, 70%, 50%)' },
+          { 'id': 'servers', 'data': serverData, 'color': '#f47DA4' }
         ],
         loading: false
       })
     }
   }
 
-  async function getStats(date,region){
-    let res = await fetch(`https://${region}.csmm.app/api/stats?since=${date.valueOf()}`)
-    let json = await res.json()
-    let regionData = []
+  async function getStats(date, region) {
+    const res = await fetch(`https://${region}.csmm.app/api/stats?since=${date.valueOf()}`)
+    const json = await res.json()
+    const regionData = []
     json.map((day) => {
-      regionData.push({players: day.players, servers: day.servers, date: day.createdAt})
+      regionData.push({ players: day.players, servers: day.servers, date: day.createdAt })
     })
     return regionData
   }
 
-  function setSelectRegion(e){
+  function setSelectRegion(e) {
     const el = e.currentTarget
     el.classList.contains('selected') ? el.classList.remove('selected') : el.classList.add('selected')
     getChartData()
   }
 
-  function getSelectedRegions(){
-    let r = []
+  function getSelectedRegions() {
+    const r = []
     const selectedRegions = document.querySelectorAll('.region.selected')
-    for(let i=0;i<selectedRegions.length;i++){
+
+    for (let i = 0; i < selectedRegions.length; i++) {
       r.push(selectedRegions[i].dataset.region)
     }
     return r
@@ -139,8 +147,8 @@ export default function Server(){
   return (
     <Container>
       <h2>Servers & Players.</h2>
-      <Regions>{REGIONS.map((region) => <Region key={region} className="region" data-region={region} onClick={setSelectRegion}>{region}</Region>)}</Regions>
-      <Inner>{chartData.loading ? <Loading/> : <Chart data={chartData.data}/>}
+      <Regions>{REGIONS.map((region) => <Region className="region" data-region={region} key={region} onClick={setSelectRegion}>{region}</Region>)}</Regions>
+      <Inner>{chartData.loading ? <Loading /> : <Chart data={chartData.data} />}
       </Inner>
     </Container>
   )
