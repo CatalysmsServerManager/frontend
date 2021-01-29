1dc2557e362e5e9d9a55ce6ed1dba8e05057096a
Niek Candaele <niekcandaele@gmail.com>
2021-01-29 10:43:49 +0100

    Linting

--- packages/server/server/src/routes/index.ts
+++ packages/server/server/src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.send('Hello 🗺')
 });
 
