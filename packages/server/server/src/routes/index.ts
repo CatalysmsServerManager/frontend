314188451feb76cda4924a33fda5e821661908e9
Niek Candaele <niekcandaele@gmail.com>
2021-01-29 10:38:06 +0100

    Cleaning some stuff

--- packages/server/server/src/routes/index.ts
+++ packages/server/server/src/routes/index.ts
@@ -2,8 +2,8 @@ import express from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.send('Hello 🗺')
 });
 
-export default  router;
+export default router;
