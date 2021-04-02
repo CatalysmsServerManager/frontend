bb65f404d197e983ed0fc9d21f734a5d61afacea
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-02 22:08:17 +0200

    Create: <Checkbox/> + Remove deprecated accordion component

---
+++ packages/ui/src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { styled } from 'styled';
+import { Checkbox, CheckboxProps } from 'components';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 2rem;
+  padding: 5rem;
+  background: ${({ theme }) => theme.colors.background};
+  border-radius: 1rem;
+`;
+
+export default {
+  title: 'Components/Form/Checkbox',
+  component: Checkbox,
+  decorators: [story => <Wrapper>{story()}</Wrapper>]
+} as Meta;
+
+const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
+
+// Default Checkbox
+export const Default = Template.bind({});
+Default.args = { name: 'checkbox01' };
+
+export const DefaultChecked = Template.bind({});
+DefaultChecked.args = { name: 'checkbox02', defaultChecked: true };
+
+export const Loading = Template.bind({});
+Loading.args = { name: 'checkbox03', loading: true };
+
+export const ReadOnly = Template.bind({});
+ReadOnly.args = { name: 'checkbox04', readOnly: true };
---
+++ packages/ui/src/components/Divider/Divider.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { styled } from 'styled';
+import { Divider, DividerProps } from 'components';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-gap: 2rem;
+  padding: 5rem;
+  background: ${({ theme }) => theme.colors.background};
+  border-radius: 1rem;
+
+  div {
+    button {
+      margin: 15px 0;
+    }
+  }
+`;
+
+export default {
+  title: 'Components/Divider',
+  component: Divider,
+  decorators: [story => <Wrapper>{story()}</Wrapper>]
+} as Meta;
+
+const Template: Story<DividerProps> = (args) => <Divider {...args} />;
+
+// Default Button
+export const Basic = Template.bind({});
+Basic.args = {};
