9aa3271c38d7b6e07e117dd7b8650cebe30fec79
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-02 21:56:41 +0200

    Update: structure of theme

--- packages/ui/src/styled/theme.ts
+++ packages/ui/src/styled/theme.ts
@@ -1,13 +1,24 @@
 export const theme = {
-  primary: '#f6d365',
-  secondary: '#fda085',
-  gray: '#d3d3d3',
-  background: '#e8edf5',
-  complement: '#1683F7',
-  gradient: 'linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)',
-  shadow: 'rgb(0 0 0 / 10%) 0px 15px 45px 0px',
-  error: '#FF4252',
-  text: '#d5dae0'
+  colors: {
+    primary: '#f6d365',
+    secondary: '#fda085',
+    tertiary: '#be81f6',
+    quaternary: '#65f6e8',
+    gray: '#d3d3d3',
+    background: '#e8edf5',
+    shadow: 'rgb(0 0 0 / 10%) 0px 15px 45px 0px',
+    error: '#FF4252',
+    text: '#d5dae0',
+  },
+  gradient: {
+    primary: 'linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%)',
+    secondary: 'linear-gradient(to right, #65f6e8 0%,#be81f6  51%, #65f6e8 100%)'
+  },
+  spacing: {
+    small: '1rem',
+    medium: '1.5rem',
+    large: '2rem'
+  }
 };
 
 export type ThemeType = typeof theme;
