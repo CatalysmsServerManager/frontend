c182e915ed34a3f9136ab8e0591b53419fec3be9
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-05-19 12:54:36 +0200

    Refactor: <Chip/>

--- packages/ui/src/components/Chip/Chip.stories.tsx
+++ packages/ui/src/components/Chip/Chip.stories.tsx
@@ -4,7 +4,7 @@ import { Avatar, Chip, ChipProps } from 'components';
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto auto;
+  grid-template-columns: auto auto auto;
   grid-gap: 2rem;
   padding: 5rem;
   background: ${({ theme }) => theme.colors.background};
@@ -19,10 +19,10 @@ export default {
 } as Meta;
 
 const Template: Story<ChipProps> = (args) => <Chip {...args} />;
-export const Basic = Template.bind({});
-Basic.args = {
+export const Default = Template.bind({});
+Default.args = {
   label: 'basic',
-  color: 'default',
+  color: 'gray',
   variant: 'default',
   onDelete: undefined,
   onClick: undefined
@@ -31,18 +31,20 @@ Basic.args = {
 export const Examples = () => (
   <>
     <Chip label="Default Chip" />
-    <Chip color="primary" label="Primary Chip" />
-    <Chip color="secondary" label="Secondary Chip" />
-    <Chip color="gradient" label="Gradient Chip" />
+    <Chip color="primary" label="Primary default Chip" />
+    <Chip color="secondary" label="Secondary default Chip" />
 
     <Chip label="Outlined Chip" variant="outline" />
-    <Chip color="primary" label="Primary Chip" variant="outline" />
-    <Chip color="secondary" label="Secondary Chip" variant="outline" />
-    <Chip color="gradient" label="Gradient Chip" variant="outline" />
+    <Chip color="primary" label="Primary outlined Chip" variant="outline" />
+    <Chip color="secondary" label="Secondary outlined Chip" variant="outline" />
 
     <Chip label="Clickable Chip" onClick={() => { }} />
     <Chip label="Deletable Chip" onDelete={() => { }} />
     <Chip avatar={<Avatar alt="avatar" size="small">NC</Avatar>} label="Avatar Chip" />
+
+    <Chip dot label="Chip with dot" />
+    <Chip color="primary" dot label="Chip with dot" />
+    <Chip color="secondary" dot label="Chip with dot" variant="outline" />
 
   </>
 );
--- packages/ui/src/components/Chip/index.tsx
+++ packages/ui/src/components/Chip/index.tsx
@@ -1,109 +1,34 @@
+// TODO: refactor component
+// TODO: add boolean dot prop
+
 import { FC, ReactNode } from 'react';
-import styled from 'styled';
 import { AiOutlineClose as Icon } from 'react-icons/ai';
+import { Container, Dot } from './style';
+import { Color, Variant } from 'styled/types';
 
-const Container = styled.div<{ disabled: boolean, color: string, outline: boolean, hasAvatar: boolean, clickable: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: fit-content;
-  padding: 3px 12px;
-  border-radius: 1.5rem;
-  cursor: ${({ clickable }): string => clickable ? 'pointer' : 'auto'};
-
-
-  svg {
-    transform: rotate(45deg);
-    ${({ theme, color, outline }) => {
-    if (!outline) {
-      return 'fill: white; stroke: white;';
-    }
-
-    switch (color) {
-      case 'default':
-        return 'fill: white; stroke: white;';
-      case 'primary':
-        return `fill: ${theme.colors.primary}; stroke: ${theme.colors.primary}`;
-      case 'secondary':
-        return `fill: ${theme.colors.secondary}; stroke: ${theme.colors.secondary}`;
-      case 'gradient':
-        return 'fill: white; stroke: white;';
-    }
-  }}
-  }
-
-  ${({ theme, color, outline }): string => {
-    if (!outline) {
-      return 'border: 2px solid transparent;';
-    }
-
-    switch (color) {
-      case 'default':
-        return `border: 2px solid ${theme.colors.gray};`;
-      case 'primary':
-        return `border: 2px solid ${theme.colors.primary};`;
-      case 'secondary':
-        return `border: 2px solid ${theme.colors.secondary};`;
-      case 'gradient':
-        return `border: 2px solid ${theme.gradient.primary};`;
-    }
-    return `border: 2px solid ${theme.colors.primary};`;
-  }}
-
-  span {
-    margin-left: ${({ hasAvatar }) => hasAvatar ? '5px' : 0};
-    font-weight: 600;
-
-    ${({ theme, color, outline }) => {
-    if (!outline) {
-      return 'color: white;';
-    }
-
-    switch (color) {
-      case 'default':
-        return `color: ${theme.colors.gray};`;
-      case 'primary':
-        return `color: ${theme.colors.primary};`;
-      case 'secondary':
-        return `color: ${theme.colors.secondary};`;
-      case 'gradient':
-        return `color: ${theme.gradient.primary};`;
-    }
-  }}
-  }
-
-
-
-  ${({ theme, color, outline }): string => {
-    if (outline) {
-      return 'background-color: transparent;';
-    }
-
-    switch (color) {
-      case 'default':
-        return `background-color: ${theme.colors.gray};`;
-      case 'primary':
-        return `background-color: ${theme.colors.primary};`;
-      case 'secondary':
-        return `background-color: ${theme.colors.secondary};`;
-      case 'gradient':
-        return `background: ${theme.gradient.primary};`;
-    }
-    return `background-color: ${theme.colors.gray};`;
-  }}
-`;
+export type ChipColor = Color | 'gray';
 
 export interface ChipProps {
   label: string;
   disabled?: boolean;
   avatar?: ReactNode;
+  dot?: boolean;
   onClick?: () => void;
   onDelete?: () => void;
-  color?: 'default' | 'primary' | 'secondary' | 'gradient';
-  variant?: 'default' | 'outline'
+  color?: ChipColor;
+  variant?: Variant
 }
 
-export const Chip: FC<ChipProps> = ({ avatar, color = 'default', variant = 'default', label, disabled = false, onClick, onDelete }) => {
+export const Chip: FC<ChipProps> = ({
+  avatar,
+  color = 'gray',
+  variant = 'default',
+  label,
+  disabled = false,
+  onClick,
+  onDelete,
+  dot
+}) => {
   return (
     <Container
       clickable={onClick !== undefined}
@@ -113,6 +38,7 @@ export const Chip: FC<ChipProps> = ({ avatar, color = 'default', variant = 'defa
       onClick={onClick ? onClick : undefined}
       outline={variant !== 'default'}
     >
+      { dot && <Dot color={color} outline={variant !== 'default'} />}
       {avatar}
       <span>{label}</span>
       { onDelete && <Icon onClick={onDelete} size={14} />}
