25f76b0e460d2be63da99baf192b21ff46f8e4d4
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-06-01 20:12:20 +0200

    Refactor <Divider/> default story

--- packages/ui/src/components/Divider/Divider.stories.tsx
+++ packages/ui/src/components/Divider/Divider.stories.tsx
@@ -1,30 +1,17 @@
 import { Meta, Story } from '@storybook/react';
-import styled from 'styled';
 import { Divider, DividerProps } from 'components';
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-  grid-gap: 2rem;
-  padding: 5rem;
-  background: ${({ theme }) => theme.colors.background};
-  border-radius: 1rem;
-
-  div {
-    button {
-      margin: 15px 0;
-    }
-  }
-`;
 
 export default {
   title: 'Components/Divider',
   component: Divider,
-  decorators: [story => <Wrapper>{story()}</Wrapper>]
 } as Meta;
 
-const Template: Story<DividerProps> = (args) => <Divider {...args} />;
-
-// Default Button
-export const Basic = Template.bind({});
-Basic.args = {};
+export const Default: Story<DividerProps> = () => (
+  <div>
+    <div>this is the content above</div>
+    <Divider
+      label={{ text: 'label', labelPosition: 'center' }}
+    />
+    <div> this is the content after</div>
+  </div>
+);
