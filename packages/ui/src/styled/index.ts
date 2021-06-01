e79dd704aabe5d8f15e9060518aba7b221ee3494
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-06-01 22:26:41 +0200

    Multiple fixes

--- packages/ui/src/styled/index.ts
+++ packages/ui/src/styled/index.ts
@@ -1,4 +1,5 @@
 /* Re-export of styled-components styled default export which at typings for the custom theme */
 import { styled } from './theme';
+export type { ThemeType } from './theme';
 
 export default styled;
