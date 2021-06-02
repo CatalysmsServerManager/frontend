b55b3ddc1c266387c21bee484eca47e7dd3cc331
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-06-03 01:54:34 +0200

    Refactor: handle payment states

--- packages/payments/src/router.tsx
+++ packages/payments/src/router.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Signin, Frame, Dashboard, Redirect, MoreInformation, Orders, PaymentState, Products, Profile, DeployServer, PaymentComplete, DiscordReturn, PageNotFound } from './pages';
+import { Signin, Frame, Dashboard, Redirect, MoreInformation, Orders, PaymentState, Products, Profile, DeployServer, DiscordReturn } from './pages';
+import { Error404 } from '@csmm/ui';
 import { AuthenticatedRoute } from './components';
 
 export const Router: FC = () => (
@@ -13,15 +14,14 @@ export const Router: FC = () => (
         <Route element={<Products />} path="/products" />
         <Route element={<Profile />} path="/profile" />
         <Route element={<Orders />} path="/orders" />
-        <Route element={<PaymentState />} path="/payment-state" />
-        <Route element={<PaymentComplete />} path="/payment-complete" />
+        <Route element={<PaymentState />} path="/payment/:state" />
       </AuthenticatedRoute>
 
       <AuthenticatedRoute element={<DeployServer />} path="/deploy/:subscriptionId" />
       <AuthenticatedRoute element={<DiscordReturn />} path="/discord/return" />
       <Route element={<MoreInformation />} path="/more-information" />
-      {/* Page not found (should stay at bottom)*/}
-      <Route element={<PageNotFound />} path="*" />
+      {/* Page not found matches with everything => should stay at bottom */}
+      <Route element={<Error404 />} path="*" />
     </Routes>
   </BrowserRouter>
 );
