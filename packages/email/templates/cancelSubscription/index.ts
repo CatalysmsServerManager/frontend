5aaf40bea96fdc615ded99564f74658d578336dc
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-07-09 14:22:56 +0200

    Add first part of cancelSubscription mail

--- packages/email/templates/cancelSubscription/index.ts
+++ packages/email/templates/cancelSubscription/index.ts
@@ -2,6 +2,8 @@ import { Template } from '../../template.type';
 
 interface CancelSubscriptionData {
   name: string;
+  daysLeft: number;
+  undoCancelLink: string;
 }
 export interface CancelSubscription extends Template {
   name: 'cancelSubscription',
