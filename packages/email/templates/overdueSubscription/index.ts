d6ab4884501f86ae5aa994116877f4425cfe7967
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-07-09 14:22:45 +0200

    Add part one of overdue subscription email

--- packages/email/templates/overdueSubscription/index.ts
+++ packages/email/templates/overdueSubscription/index.ts
@@ -2,6 +2,7 @@ import { Template } from '../../template.type';
 
 interface Data {
   name: string;
+  paymentLink: string;
 }
 export interface OverdueSubscription extends Template {
   name: 'overdueSubscription',
