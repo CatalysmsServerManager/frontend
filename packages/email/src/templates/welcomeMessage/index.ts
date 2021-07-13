32341ae107ed9cda1300c07cbeb89a72449a676e
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-07-13 20:54:35 +0200

    Move to src + minor changes

---
+++ packages/email/src/templates/welcomeMessage/index.ts
@@ -0,0 +1,9 @@
+import { Template } from '../../../template.type';
+
+interface Data {
+  name: string;
+}
+export interface welcomeMessage extends Template {
+  name: 'welcomeMessage',
+  data: Data;
+}
--- packages/email/templates/welcomeMessage/index.ts
+++
@@ -1,9 +0,0 @@
-import { Template } from '../../template.type';
-
-interface Data {
-  name: string;
-}
-export interface OverdueSubscription extends Template {
-  name: 'welcomeMessage',
-  data: Data;
-}
