d9df0740b7325a56224043cfc0a5fdf3d51a9a90
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-07-13 21:49:26 +0200

    Change console log with winston

--- packages/email/src/render.ts
+++ packages/email/src/render.ts
@@ -1,20 +1,34 @@
 import ejs from 'ejs';
+import * as fs from 'fs/promises';
 import mjmml2html from 'mjml';
+import winston from 'winston';
 
 import { Template } from '../template.type';
 import { Generic } from './templates/generic';
 import { welcomeMessage } from './templates/welcomeMessage';
 
+// Use default logger and set transporter instead of using custom logger.
+const console = new winston.transports.Console();
+winston.add(console);
+
 export async function render(template: Generic): Promise<string>;
 export async function render(template: welcomeMessage,): Promise<string>;
 export async function render(template: Template): Promise<string> {
-  // changes variables with custom data.
-  const result = await ejs.renderFile(`src/templates/${template.name}/template.ejs`, template.data, { async: true });
-  const out = mjmml2html(result);
+  const path = `src/templates/${template.name}/template.ejs`;
 
-  if (out.errors.length !== 0) {
-    // change this to logger.
-    console.error('Email generated the following errors', out.errors);
+  // this is probably lil weird no? @niekcandaele
+  try {
+    fs.access(path);
+  } catch (error) {
+    winston.error(error);
+    winston.error('The requested template does not exist.');
+  }
+
+  const result = await ejs.renderFile(path, template.data, { async: true });
+  const out = mjmml2html(result);
+  if (out.errors.length > 0) {
+    winston.error(`Email template ${template.name} generated the following errors: `, out.errors);
+    return '';
   }
   return out.html;
 };
