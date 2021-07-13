b345ab60620cd323b10f3648640e0d7fdea83581
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-07-13 20:52:56 +0200

    Create generic template

---
+++ packages/email/src/templates/generic/index.ts
@@ -0,0 +1,19 @@
+import { Template } from '../../../template.type';
+
+interface Button {
+  text: string;
+  href: string;
+  color: string; // Should be color interface
+}
+
+interface Data {
+  name: string;
+  paragraph_1: string;
+  paragraph_2?: string;
+  button: Button;
+}
+
+export interface Generic extends Template {
+  name: 'generic',
+  data: Data;
+}
